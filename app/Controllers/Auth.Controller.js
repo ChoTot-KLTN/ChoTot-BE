52181344100b00500e3e033eb54555c815994b1e
vinhduc5984 <phungvinhduc3@gmail.com>
2022-02-19 16:40:45 +0700

    Auth service: login , verify account

--- app/Controllers/Auth.Controller.js
+++ app/Controllers/Auth.Controller.js
@@ -1,6 +1,6 @@
 const { encodedToken } = require("../Middlewares/Token.Middleware");
 const { sendError, sendSuccess } = require("./Controller");
-const {register} = require("../Services/Auth.Service");
+const {register, login, verifydAccount} = require("../Services/Auth.Service");
 
 const handleRegister = async (req, res) => {
     const result = await register(req.body);
@@ -9,6 +9,32 @@ const handleRegister = async (req, res) => {
     return sendError(res, result.message, result.status);
   };
 
+const handleLogin = async (req,res)=>{
+  const result = await login(req.body);
+  if(result.success){
+    const accessToken = encodedToken(result.data.idUser);
+    const role = result.data.role;
+    res.setHeader("Authorization", accessToken);
+    return sendSuccess(
+      res,
+      { accessToken: accessToken, role: role },
+      result.message,
+      result.status
+    );
+  }
+  return sendError(res, result.message, result.status);
+}
+
+const handleVerify = async (req,res)=>{
+  const result = await verifydAccount(req.body);
+  if(result.success){
+    return sendSuccess(res, result.data, result.message, result.status);
+  }
+  return sendError(res, result.message, result.status);
+};
+
 module.exports = {
     handleRegister,
+    handleLogin,
+    handleVerify
 }
--- app/Services/Auth.Service.js
+++ app/Services/Auth.Service.js
@@ -67,6 +67,117 @@ const register = async (body)=>{
     }
 };
 
+const login = async (body)=>{
+    const {email,password} = body;
+    try {
+
+        const account = await Account.findOne({username:email});
+        if(!account){
+            return {
+                message: {
+                  ENG: "Account or password is incorrect",
+                  VN: "Tài khoản hoặc mật khẩu không chính xác",
+                },
+                success: false,
+                status: HTTP_STATUS_CODE.FORBIDDEN,
+              };
+        }
+        const isCorrectPassword = await bcrypt.compare(password,account.password);
+        if(!isCorrectPassword){
+            return{
+                message:{
+                    ENG:"Password is incorrect",
+                    VN:"Sai mật khẩu",
+                },
+                success: false,
+                status: HTTP_STATUS_CODE.NOT_FOUND
+            };
+        }
+        if (account.isVerified === false) {
+            const otp = await generateString(4, false);
+            await sendOtp(account.username, otp);
+            account.otp = otp;
+            await account.save();
+            return {
+              message: {
+                ENG: "Please verify your account. A verify code has been sent to your email",
+                VN: "Hãy kích hoạt tài khoản của bạn. Một mã otp đã được gửi đến email của bạn",
+              },
+              success: false,
+              status: HTTP_STATUS_CODE.UNAUTHORIZED,
+            };
+          }
+          return {
+            message: {
+              ENG: "Login Successfully",
+              VN: "Đăng nhập thành công",
+            },
+            data: {
+              idUser: account.idUser,
+              role: account.role,
+            },
+            success: true,
+            status: HTTP_STATUS_CODE.OK,
+          };
+    }catch(error){
+        return {
+            success: false,
+            message: error.message,
+            status: error.status,
+          };
+    }
+};
+
+const verifydAccount =async (body)=>{
+  try{
+    const {email, otp} = body;
+    const account = await Account.findOne({username:email});
+    if(!account){
+      return {
+        success: false,
+        message: {
+          ENG: "Account not found",
+          VN: "Tài khoản không tồn tại",
+        },
+        status: HTTP_STATUS_CODE.NOT_FOUND,
+      };
+    }
+    if(account.otp === otp)
+    {
+      account.isVerified = true;
+      account.otp = "";
+      await account.save();
+    }
+    if(account.isVerified === true){
+      return {
+        data: "data",
+        success: true,
+        message: {
+          ENG: "Your account is verified",
+          VN: "Tài khoản của bạn đã được kích hoạt",
+        },
+        status: HTTP_STATUS_CODE.OK,
+      }
+    }
+    return {
+      success: false,
+      message: {
+        ENG: "Invalid otp",
+        VN: "Mã otp không hợp đúng",
+      },
+      status: HTTP_STATUS_CODE.UNAUTHORIZED,
+    };
+  }catch(error){
+    return {
+      success: false,
+      message: error.message,
+      status: error.status,
+    };
+  }
+};
+
 module.exports = {
-    register
+    register,
+    login,
+    verifydAccount
 }
