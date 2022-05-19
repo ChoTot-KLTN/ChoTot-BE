8a999269364547d29658d18ce79e2354ac0551b5
vinhduc5984 <phungvinhduc3@gmail.com>
2022-05-20 06:57:55 +0700

    fix: deploy heroku 1

--- app/Common/Twilio_SMS.js
+++ app/Common/Twilio_SMS.js
@@ -2,8 +2,7 @@ const twilio = require('twilio');
 const {AccountSID, AuthToken, TWILIO_PHONENUMBER} = require("../Common/Config");
 const client = new twilio(AccountSID, AuthToken);
 const sendSMS =async (data) =>  {
-    console.log("AccountSID: ",AccountSID);
-    console.log("AuthToken: ",AuthToken);
+   
   await client.messages
   .create({
     body: data.message,
